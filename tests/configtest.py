364cc919017ce41cc36b20a204d0a98c6368c124
robin <robintoor85@gmail.com>
2025-04-27 11:08:26 +1000

    browserInstance fixture added

--- tests/configtest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.fixture(scope="session")
-def login_details(request):
-    return request.param
-
--- tests/test_loginPage.py
+++ tests/test_loginPage.py
@@ -1,7 +1,6 @@
 import json
 import pytest
 from playwright.sync_api import Page, expect, Playwright
-from pytest_playwright.pytest_playwright import browser, new_context
 from pageObjects.loginPage import LoginPage
 
 with open("../testData/credentials.json") as j:
@@ -10,10 +9,9 @@ with open("../testData/credentials.json") as j:
 
 
 @pytest.mark.parametrize('login_details', user_credentials_list)
-def test_loginToTheStoreWebsite(playwright: Playwright, login_details):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def test_loginToTheStoreWebsite(playwright: Playwright,browserInstance, login_details):
+
+    page = browserInstance
     username = login_details['username']
     password = login_details['password']
 
@@ -25,7 +23,4 @@ def test_loginToTheStoreWebsite(playwright: Playwright, login_details):
     #assert
     expect(page.get_by_role("heading", name="My Account")).to_be_visible()
 
-    # closing browser
-    context.close()
-    browser.close()
 
